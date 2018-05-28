3fc3121d163ab652287776c784cedffb9216f2bb
RFrankowski <r.franek93@gmail.com>
2018-05-28 13:15:22 +0200

    nowe

--- nowy_genetyk_licz.py
+++ nowy_genetyk_licz.py
@@ -3,15 +3,14 @@ import random
 import math
 import numpy as np
 
+random.seed(0)
+
 
 class Miasto():
     def __init__(self):
         self.x = random.randint(0, 500)
         self.y = random.randint(0, 500)
 
-        # self.y = random.randint(10, 500)
-        #
-        # self.y = random.randint(0, 20)
 
 def generuj_populacjie_starowa(ile_miast, poczatek, ile_populacji):
     populacja = []
@@ -20,34 +19,37 @@ def generuj_populacjie_starowa(ile_miast, poczatek, ile_populacji):
         for miasto in range(ile_miast):
             if poczatek != miasto:
                 osobnik.append(miasto)
-
         random.shuffle(osobnik)
         osobnik.insert(0, poczatek)
         osobnik.append(poczatek)
-
         populacja.append(osobnik)
     return populacja
 
 
-# print generuj_populacjie_starowa(3, 1, 1)
+# print generuj_populacjie_starowa(3,2,1)
 
 
-def alg_genetyczny(ile_miast, poczatek, ile_populacji):
-    populacja_startowa = generuj_populacjie_starowa(ile_miast, poczatek, ile_populacji)
-
+def genetyk(ile_miast):
+    pop_start = generuj_populacjie_starowa(2, 2, 1)
+    print(pop_start)
     miasta = []
-    for miasto in range(ile_miast):
+    for i in range(ile_miast):
         miasta.append(Miasto())
 
     for i in miasta:
-        print(i.x, i.y)
+        print i.x, i.y
 
-    dlugosc = 0
-    for i in range(ile_miast):
-        if i != ile_miast - 1:
-            dlugosc += math.sqrt(abs((miasta[i + 1].x - miasta[i].x) ^ 2 + (miasta[i + 1].y - miasta[i].y) ^ 2))
+    dlugos = 0
+    for i in enumerate(pop_start[0]):
+        if i[0] != len(pop_start[0]) - 1:
+            miasto1 = pop_start[0][i[0]]
+            miasto2 = pop_start[0][i[0] + 1]
+            # print pop_start[0][i[0]], pop_start[0][i[0]+1]
 
-    print(dlugosc)
+            dlugos += math.sqrt(
+                abs((miasta[miasto2].x - miasta[miasto1].x) ^ 2 + (miasta[miasto2].y - miasta[miasto1].y) ^ 2))
+
+    print dlugos
 
 
-alg_genetyczny(3, 1, 1)
+genetyk(3)
